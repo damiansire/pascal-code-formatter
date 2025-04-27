a74008a087f779de0e21e658ba7c89381cd693b4
Damian Sire <damiansire@gmail.com>
2025-04-27 20:41:46 +0700

    fix: add optional chaining to prevent runtime errors in isComment function

--- src/shared/libs.ts
+++ src/shared/libs.ts
@@ -1,7 +1,7 @@
 import { PascalToken } from "pascal-tokenizer";
 
 const isComment = (token: PascalToken): boolean => {
-  return ["COMMENT_STAR", "COMMENT_BLOCK_BRACE", "COMMENT_LINE"].includes(token.type);
+  return ["COMMENT_STAR", "COMMENT_BLOCK_BRACE", "COMMENT_LINE"].includes(token?.type);
 };
 
 export { isComment };
