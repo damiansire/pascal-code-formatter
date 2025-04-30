f8b16660a1e19f0ef06602d38e55945926a7c763
Damian Sire <damiansire@gmail.com>
2025-04-30 16:38:48 +0700

    feat: add new algorithms for formatter

--- src/logic/formatter.controller.ts
+++ src/logic/formatter.controller.ts
@@ -1,44 +1,61 @@
 import { PascalToken } from "pascal-tokenizer";
-import { FormattedPascalLine } from "../shared/types";
+import { InternalFormattedPascalLine } from "../shared/types";
 import { EmptyLine, WhiteSpace } from "../shared/elements";
 import { IdentationManager } from "./identation-manager";
+import { isEndOfLine, needWhiteSpace } from "../shared/libs";
 
 class FormatterController {
   private currentLineTokens: PascalToken[];
-  private formattedLines: FormattedPascalLine[];
+  private formattedLines: InternalFormattedPascalLine[];
   private indentationManager: IdentationManager;
 
-  constructor() {
+  constructor(tokens: PascalToken[]) {
     this.currentLineTokens = [];
     this.formattedLines = [];
     this.indentationManager = new IdentationManager();
+
+    for (let index = 0; index < tokens.length; index++) {
+      const prevToken = tokens[index - 1];
+      const currentToken = tokens[index];
+      const nextToken = tokens[index + 1];
+      this.processToken(prevToken, currentToken, nextToken);
+    }
   }
 
-  public addTokenToCurrentLine(token: PascalToken): void {
+  private processToken(prevToken: PascalToken, currentToken: PascalToken, nextToken: PascalToken) {
+    this.addTokenToCurrentLine(currentToken);
+
+    const addWhiteSpace = needWhiteSpace(currentToken, nextToken);
+    if (addWhiteSpace) {
+      this.addWhiteSpace();
+    }
+
+    const finishLine = isEndOfLine(currentToken, nextToken)
+    if (finishLine) {
+      this.finalizeLine();
+    }
+  }
+
+  private addTokenToCurrentLine(token: PascalToken): void {
     this.currentLineTokens.push(token);
   }
 
-  public finalizeLine(): void {
+  private finalizeLine(): void {
     if (this.currentLineTokens.length > 0) {
       const deepLine = this.indentationManager.evaluateLineIndentation(this.currentLineTokens);
       this.formattedLines.push({
         tokens: [...this.currentLineTokens],
         indentation: deepLine,
-        type: "ASSIGNMENT",
       });
       this.currentLineTokens = [];
     }
   }
 
-  public addEmptyLine(): void {
-    this.formattedLines.push(EmptyLine);
-  }
-
-  public addWhiteSpace() {
+  private addWhiteSpace() {
     this.currentLineTokens.push(WhiteSpace);
   }
 
-  public getFormattedLines(): FormattedPascalLine[] {
+  public getFormattedLines(): InternalFormattedPascalLine[] {
     return this.formattedLines;
   }
 }
